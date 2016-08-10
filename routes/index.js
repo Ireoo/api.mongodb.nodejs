fd7f1a8c871fa80b9011cc6a3294e3d0ae1af09c
S2 <i@ireoo.com>
2016-08-11 03:31:21 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,10 @@ var password = process.env.MONGODB_PASSWORD;
 var username = process.env.MONGODB_USERNAME;
 
 /**
- * 设置mongodb数据库路径
- * @mongodb {string}
+ * 设置mongodb数据库连接
+ * @type {mongojs}
  */
-var mongodb  = "localhost/ireoo";
-
+var mongodb  = mongojs("localhost/api");
 
 /**
  * 初始化颜色主题
@@ -51,17 +50,37 @@ exports.index = function(req, res, next) {
  * @param next
  */
 exports.mongoDB = function(req, res, next) {
-    var db = eval("mongojs('" + mongodb + "', ['" + req.params.key + "'])." + req.params.key);
-    var input = JSON.parse(req.input);
-    console.log("[input]  --> ".info + JSON.stringify(input).input);
-    // console.log("mongojs('" + mongodb + "', ['" + req.params.key + "'])." + req.params.key);
-    // console.log(db);
+    /**
+     * 切换到 {req.params.key} 数据表
+     */
+    var db = eval("mongodb." + req.params.key);
 
+    /**
+     * 格式化数据流数据为JSON格式
+     * @type {{}}
+     */
+    var input = JSON.parse(req.input);
+
+    /**
+     * 调试输出获取的数据流信息
+     */
+    console.log("[input]  --> ".info + JSON.stringify(input).input);
+
+    /**
+     * 格式化数据流里各项参数where, data, other为JSON格式
+     * @type {{}}
+     */
     var where = JSON.stringify(input.where)=='[]' || !input.where ? {} : input.where;
     var data  = JSON.stringify(input.data)=='[]' || !input.data ? {} : input.data;
     var other = JSON.stringify(input.other)=='[]' || !input.other ? {} : input.other;
 
+    /**
+     * 主体程序入口处
+     */
     switch (req.params.mode) {
+        /**
+         * 执行插入命令
+         */
         case 'insert':
             db.insert(data, function(err, result) {
                 console.log("[output] --> ".info + (err ? JSON.stringify(err).error : JSON.stringify(result).data));
@@ -69,6 +88,9 @@ exports.mongoDB = function(req, res, next) {
             });
             break;
 
+        /**
+         * 执行查找命令
+         */
         case 'find':
             db.find(where).sort(other, function(err, result) {
                 console.log("[output] --> ".info + (err ? JSON.stringify(err).error : JSON.stringify(result).data));
@@ -76,6 +98,9 @@ exports.mongoDB = function(req, res, next) {
             });
             break;
 
+        /**
+         * 执行查找一条数据命令
+         */
         case 'findone':
             db.findOne(where, function(err, result) {
                 console.log("[output] --> ".info + (err ? JSON.stringify(err).error : JSON.stringify(result).data));
@@ -83,6 +108,9 @@ exports.mongoDB = function(req, res, next) {
             });
             break;
 
+        /**
+         * 执行修改数据命令
+         */
         case 'update':
             db.update(where, {$set: data}, other, function(err, result) {
                 console.log("[output] --> ".info + (err ? JSON.stringify(err).error : JSON.stringify(result).data));
@@ -90,6 +118,9 @@ exports.mongoDB = function(req, res, next) {
             });
             break;
 
+        /**
+         * 执行修改叠加命令
+         */
         case 'plus':
             db.update(where, {$inc: data}, other, function(err, result) {
                 console.log("[output] --> ".info + (err ? JSON.stringify(err).error : JSON.stringify(result).data));
@@ -97,6 +128,9 @@ exports.mongoDB = function(req, res, next) {
             });
             break;
 
+        /**
+         * 执行删除命令
+         */
         case 'remove':
             db.remove(data, other, function(err, result) {
                 console.log("[output] --> ".info + (err ? JSON.stringify(err).error : JSON.stringify(result).data));
@@ -104,6 +138,9 @@ exports.mongoDB = function(req, res, next) {
             });
             break;
 
+        /**
+         * 获取该表状态信息
+         */
         case 'stats':
             db.stats(function (err, result) {
                 console.log("[output] --> ".info + (err ? JSON.stringify(err).error : JSON.stringify(result).data));
@@ -111,6 +148,9 @@ exports.mongoDB = function(req, res, next) {
             });
             break;
 
+        /**
+         * 获取指定条件下数据量
+         */
         case 'count':
             db.count(where, function(err, result) {
                 console.log("[output] --> ".info + (err ? JSON.stringify(err).error : JSON.stringify(result).data));
@@ -118,8 +158,11 @@ exports.mongoDB = function(req, res, next) {
             });
             break;
 
+        /**
+         * 当不存在该指令时返回404
+         */
         default:
-            res.send("404 NOT FOUND");
+            res.send(404);
             break;
     }
 
