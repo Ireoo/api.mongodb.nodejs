3586a3ebbc7a1e49ef9fef3b479086205a756391
s2 <s@ireoo.com>
2017-07-07 18:22:33 +0800

    update

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(function (req, res, next) {
     });
     req.on('end', () => {
         // console.log('>>>req end');
+        console.log(Buffer.concat(reqData, size));
         req.input = JSON.parse(Buffer.concat(reqData, size) || "{}");
     });
     next();
